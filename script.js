f01eada2ce13245417655755596e5b1629cc6eaf
Prajwal Nevase <prajwalnevase04@gmail.com>
2026-07-13 20:02:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,276 @@
+// console.log("Hello Prajwal")
+
+
+// // ! var declaration type
+
+// var a;
+// console.log(a)
+
+// a=10
+// console.log(a)
+
+// var b=20;
+// console.log(b)
+
+// var b;
+// console.log(b);
+
+
+
+// // ! let declaration type
+
+// let a;
+// console.log(a);
+
+// a=20;
+// console.log(a);
+
+// let b=30;
+// console.log(b);
+
+// let b;
+// console.log(b);
+
+// b=40;
+// console.log(b);
+
+
+
+// // ! const declaration type
+
+// const a=20;
+// console.log(a);
+
+
+
+// // !number type
+
+// let a=10;
+// console.log(a);
+// console.log(typeof a);
+
+// a=20.34;
+// console.log(typeof a);
+
+
+
+// // !boolean typr
+
+// let isworking = true;
+// console.log(isworking);
+
+// let ismarried = false;
+// console.log(ismarried);
+
+
+
+// // !string type
+
+// let name ='Prajwal'
+// console.log(name);
+// console.log(typeof name);
+
+// let name1="Prajwal"
+// console.log(name1);
+// console.log(typeof name1);
+
+// let name2="Prajwal"
+// console.log(name2);
+// console.log(typeof name2);
+
+// console.log('my name is $(name2)');
+
+
+
+// // ! null type
+
+// let val1=null;
+// console.log(val1);
+// console.log(typeof val1);
+
+
+
+// // ! undefined type
+
+// let val2=undefined;
+// console.log(typeof val2);
+
+// console.log("Hellow "+ "World");
+// console.log("Hellow "+ "10");
+// console.log("Hellow "+ 10);
+// console.log("Hellow "+ true);
+// console.log("Hellow "+ null);
+// let a= "Hello" + undefined
+// console.log(typeof a);
+
+// console.log(10+"10");
+// console.log(10+10);
+// console.log(10 + null);
+// console.log(10 + undefined);
+// console.log(10 + true);
+
+// console.log("Hello"=="hello");
+// console.log("10"==10);
+// console.log("10"===10);
+// console.log(10===10);
+
+
+// let age =10;
+// if (age >=18){
+//     console.log("you are eligible for voting");
+// }
+// else{
+//     console.log("Oooo... you are not eligible ");
+// }
+
+
+
+
+
+// // ......(Function)......
+
+// // !named fuction
+
+// add(3,7)
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(3,5);
+// add(27,24)
+
+
+
+// // !Anonymous function
+// function() {
+// }
+
+
+
+// // !function expression
+
+// let student= function() {
+//     console.log("Hello");
+// }
+// student()
+
+// let student1=function add(){
+//     console.log("Hellow");
+// }
+// student1()
+
+
+
+// // ! Arrow function
+
+// let add = ()=>{
+//     console.log("Hello");
+// }
+// add()
+
+// let add=()=>{return"Hello";}
+// console.log(add());
+
+// let add1=a=>a;
+// console.log(add1(5));
+
+// let add2=_=>"hello"
+// console.log(add2());
+
+// let add=(a,b) => a+b
+// console.log(add(5,5));
+
+// let sub=(a,b) => a-b
+// console.log(sub(5,5));
+
+
+
+
+// // !Higher order function  and  callback function
+
+// function addition(a,b) {
+//     return a+b
+// }
+
+// console.log(addition(5,num()));
+
+// function num() {
+//     return 5
+// }
+
+
+// -----------------------------------------------------------------------------------
+
+//  let student=[1,2,3,4];
+//  console.log(student);
+
+//  let student1=[1,"ram",true];
+//  console.log(student1);
+
+//  console.log(student1[1]);
+
+// student1[1]="sita";
+// console.log(student1);
+
+// console.log(typeof student1);
+
+// let employee=[new Array;
+// employee="Ram",5;
+
+// con
+
+
+
+
+// let number=[10,20,30,40,50,60]
+// console.log(number)
+
+// number.push(70);
+// console.log(Number);
+
+// number.pop();
+// console.log(Number);
+
+// number.unshift(90);
+// console.log(Number);
+
+// number.shift();
+// console.log(Number);
+
+// let num=number.slice(1,2);
+// console.log(num);
+
+// console.log(number.splice(1,0,90,70));
+// console.log(number);
+
+// console.log(number.reverse());
+// let a=number.join("-");
+// console.log(a);
+
+// console.log(number.toString());
+
+// console.log(number.concat(number1));
+
+// let num2 = [65,67,27,11,20,30,40]
+// console.log(num2);
+
+// let num1 = [num2.map((i)=>i*2)]
+// console.log(num1);
+
+// let even=num2.filter((i)=>{
+//     return i % 2 ==0
+// })
+
+// console.log(even);
+
+
+
+
+
+ 
+ 
+
+ 
+
+
+
+
